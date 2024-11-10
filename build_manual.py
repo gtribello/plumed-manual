aa0de3441d1ec74504ca841b96ae81474b95aa34
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-11-10 13:11:39 +0000

    Turning on building of new style manual for actions in generic module

--- build_manual.py
+++ build_manual.py
@@ -296,7 +296,7 @@ def createActionPage( version, action, value, neggs, nlessons, actdb ) :
 
          ninp, nfail = 0, 0
          f.write("## Further details and examples \n")
-         fixed_modules = ["adjmat", "envsim", "sprint", "clusters", "secondarystructure", "multicolvar", "valtools", "matrixtools", "colvar", "pamm", "volumes"]
+         fixed_modules = ["adjmat", "envsim", "sprint", "clusters", "secondarystructure", "multicolvar", "valtools", "matrixtools", "colvar", "pamm", "volumes", "generic"]
          if value["module"] in fixed_modules : 
              with open("automatic/" + action + ".md", "r") as iff : inp = iff.read()
              with open("automatic/" + action + ".md", "w+") as off : off.write( inp.replace(value["description"],"") )
