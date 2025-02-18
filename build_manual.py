a17240a011027923fd673c830800ec70c00442fc
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2025-02-18 13:10:53 +0000

    Stupid boy

--- build_manual.py
+++ build_manual.py
@@ -382,7 +382,7 @@ if __name__ == "__main__" :
    # Print the javascript for plumed to html to a file
    with open("assets/plumed.js", "w+") as jf : jf.write( get_javascript() )
    # Print the css for plumed to html to a file
-   with open("assets/plumed.css", "w+" as cf : cf.write( get_css() )
+   with open("assets/plumed.css", "w+") as cf : cf.write( get_css() )
    # Print the date to the data directory
    today = { "date": date.today().strftime('%B %d, %Y') }
    df = open("_data/date.json","w")
